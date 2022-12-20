875e140b241128474e6c2cd8562742b697ad0de5
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-12-20 12:22:01 +0700

    Update

--- codes/T07/C1.sol
+++ codes/T07/C1.sol
@@ -12,7 +12,51 @@ contract lottery {
         manager = msg.sender;
     }
 
+
     function enter() public payable {
+        require(msg.value > 0.1 ether, "Please send at least 0.1 ETH.");
         players.push(msg.sender);
     }
+
+
+    function getPlayers() public view returns(address[] memory) {
+        return players;
+    }
+
+    
+    function random() private view returns (uint256) {
+        uint256 timestamp = block.timestamp;
+        uint256 difficulty = block.difficulty;
+        // Dynamic array of byte or byte[]
+        bytes memory enc = abi.encodePacked(timestamp, difficulty);
+        bytes32 hash = keccak256(enc);
+        uint256 num = uint(hash);
+        return num ;
+    }
+
+    // Shorter version
+    // function randomShort() private view returns (uint256) {
+    //     return uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)));
+    // }
+
+    // Just for testing
+    // function get_random() public view returns(uint256) {
+    //     return random();
+    // }
+
+    // Just for testing
+    // function get_winner_idx() public view returns(uint256) {
+    //     return random() % players.length;
+    // }
+
+    function pickWinner() public {
+        require(msg.sender == manager, "You need to be a manager.");
+        require(players.length > 0, "Need at least one player.");
+        uint256 idx = random() % players.length;
+        uint256 balance = address(this).balance;
+        payable(players[idx]).transfer(balance);
+        players = new address[](0);
+    }
+
+
 }
