f37da4121221b3bf8358441a5f698c09e5252aa1
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-01-10 19:26:29 +0700

    Update

--- codes/T12/C1.sol
+++ codes/T12/C1.sol
@@ -2,40 +2,59 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 interface ERC20Interface {
     // State variables
     function name() external view returns (string memory);
+
     function symbol() external view returns (string memory);
+
     function decimals() external view returns (uint8);
+
     function totalSupply() external view returns (uint256);
 
     // Mandatory functions
     function balanceOf(address _owner) external view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) external returns (bool success);
+
+    function transfer(address _to, uint256 _value)
+        external
+        returns (bool success);
 
     // Optional functions
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
-    function approve(address _spender, uint256 _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    function approve(address _spender, uint256 _value)
+        external
+        returns (bool success);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
 
     // Events
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 }
 
 contract Cryptos is ERC20Interface {
-    string public override name = "NRTest";
-    string public override symbol = "NRTest";
-    uint8 public override decimals = 0; //18
+    string public override name = "Industrail Engineering CMU";
+    string public override symbol = "IECMU";
+    uint8 public override decimals = 0;
     uint256 public override totalSupply;
-
 
     address public founder;
     mapping(address => uint256) balances;
 
-    mapping(address => mapping(address => uint)) allowed;
+    // V2
+    mapping(address => mapping(address => uint256)) allowed;
 
     constructor() {
         totalSupply = 1000000;
@@ -43,41 +62,81 @@ contract Cryptos is ERC20Interface {
         balances[founder] = totalSupply;
     }
 
-    function balanceOf(address tokenOwner) public view override returns (uint balance) {
-        return balances[tokenOwner];
+    function balanceOf(address _owner)
+        public
+        view
+        override
+        returns (uint256 balance)
+    {
+        return balances[_owner];
     }
 
-    function transfer(address to, uint tokens) public override returns(bool success) {
-        require(balances[msg.sender] >= tokens, "You don't have enough token to send.");
+    function transfer(address _to, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
+        require(
+            balances[msg.sender] >= _value,
+            "You don't have enough money to send."
+        );
 
-        balances[to] += tokens;
-        balances[msg.sender] -= tokens;
-        emit Transfer(msg.sender, to, tokens);
+        balances[_to] += _value;
+        balances[msg.sender] -= _value;
+
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) view public override returns(uint256 remaining) {
+    // V2
+    function allowance(address _owner, address _spender)
+        public
+        view
+        override
+        returns (uint256 remaining)
+    {
         return allowed[_owner][_spender];
     }
 
-    function approve(address _spender, uint256 _value) public override returns (bool success){
+    // V2
+    function approve(address _spender, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
         require(_value > 0, "The allowed value must be greater than zero.");
-        require(balances[msg.sender] >= _value, "You don't have enough token to approve spending.");
+        require(
+            balances[msg.sender] >= _value,
+            "Not enough token in the origin account."
+        );
 
         allowed[msg.sender][_spender] = _value;
 
-        emit Approval(msg.sender,_spender, _value );
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
-        require(allowed[_from][msg.sender] >= _value, "You don't have enough allowance.");
-        require(balances[_from] >= _value, "Not enough token in the origin account.");
-        balances[_from] -= _value; 
+    // V2
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public override returns (bool success) {
+        require(
+            allowed[_from][msg.sender] >= _value,
+            "You don't have enough allowance."
+        );
+        require(
+            balances[_from] >= _value,
+            "Not enough tokens in the origin account"
+        );
+
+        balances[_from] -= _value;
         allowed[_from][msg.sender] -= _value;
         balances[_to] += _value;
 
         emit Transfer(_from, _to, _value);
+
         return true;
     }
 }
