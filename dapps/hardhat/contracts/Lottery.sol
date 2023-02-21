a9d9633cacd94627639a94ed3a3a5035a3635054
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-02-21 16:34:53 +0700

    Update

--- dapps/hardhat/contracts/Lottery.sol
+++ dapps/hardhat/contracts/Lottery.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract lottery {
+contract Lottery {
     address public manager;
     address[] public players;
 
--- dapps/hardhat/test/Lottery.ts
+++ dapps/hardhat/test/Lottery.ts
@@ -3,16 +3,22 @@ import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("Lottery", function () {
-  before(function () {
-    console.log("Before");
-  });
-  beforeEach(function () {
-    console.log("BeforeEach");
-  });
-  it("tests a", function () {
-    console.log("Ita");
-  });
-  it("tests b", function () {
-    console.log("Itb");
+  async function deployFixture() {
+    const [owner, other] = await ethers.getSigners();
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy();
+    return { owner, lottery };
+  }
+
+  describe("State Variable Checks", function () {
+    it("checks manager", async function () {
+      const { owner, lottery } = await loadFixture(deployFixture);
+      expect(await lottery.manager).to.equal(owner.address);
+    });
+
+    it("checks players array", async function () {
+      const { owner, lottery } = await loadFixture(deployFixture);
+      expect(await lottery.players(0)).to.equal("sdfsdf");
+    });
   });
 });
