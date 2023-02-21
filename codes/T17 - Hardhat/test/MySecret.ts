34f0eba9c5c99223ff71adf4a7196c70bc97fd60
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-02-21 15:37:55 +0700

    Update

--- codes/T17 - Hardhat/test/MySecret.ts
+++ codes/T17 - Hardhat/test/MySecret.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Secret Message", () => {
-  it("checks initial message", async () => {
+describe("Secret Message", function () {
+  it("checks initial message", async function () {
     const MySecret = await ethers.getContractFactory("MySecret");
     const mySecret = await MySecret.deploy("My Super Secret!");
     expect(await mySecret.secret()).to.equal("My Super Secret!");
---
+++ dapps/hardhat/test/Lottery.ts
@@ -0,0 +1,5 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+
+describe("Test", function () {});
