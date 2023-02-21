78696571bcc78f32bd6452cf4754f98b41d21af3
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-02-22 06:31:49 +0700

    Update

--- dapps/hardhat/test/Lottery.ts
+++ dapps/hardhat/test/Lottery.ts
@@ -6,19 +6,39 @@ describe("Lottery", function () {
   async function deployFixture() {
     const [owner, other] = await ethers.getSigners();
     const Lottery = await ethers.getContractFactory("Lottery");
-    const lottery = await Lottery.deploy();
-    return { owner, lottery };
+    // const lottery = await Lottery.deploy();
+    const lottery = await Lottery.connect(owner).deploy();
+    return { owner, lottery, other };
   }
 
   describe("State Variable Checks", function () {
-    it("checks manager", async function () {
+    it("checks manager - lottery", async function () {
       const { owner, lottery } = await loadFixture(deployFixture);
-      expect(await lottery.manager).to.equal(owner.address);
+      // const a = await lottery.manager();
+      // console.log(typeof a);
+      // console.log(a);
+      expect(await lottery.manager()).to.equal(owner.address);
     });
 
-    it("checks players array", async function () {
-      const { owner, lottery } = await loadFixture(deployFixture);
-      expect(await lottery.players(0)).to.equal("sdfsdf");
+    it("checks enter - lottery", async function () {
+      const { owner, lottery, other } = await loadFixture(deployFixture);
+      await expect(lottery.connect(other).enter()).to.be.reverted;
+    });
+
+    it("checks data - lottery", async function () {
+      const { owner, lottery, other } = await loadFixture(deployFixture);
+
+      // try {
+      //   await lottery.players(0);
+      // } catch (err) {
+      //   throw new Error(JSON.stringify(err));
+      // }
+
+      // await expect(lottery.players(0)).to.eventually.be.rejectedWith();
+
+      const a = await lottery.getPlayers();
+      console.log({ a });
+      expect(a.length).to.equal(0);
     });
   });
 });
